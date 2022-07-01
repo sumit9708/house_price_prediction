ac7a20b0bb32e33ef7a1670762f9dcb52ff97756
Sumit <sumitbhagat498@gmail.com>
2022-07-01 11:24:06 +0530

    Created exception and logger file completed

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from distutils.debug import DEBUG
 from flask import Flask
+from housing.logger import logging
 
 app = Flask(__name__)
 
 @app.route("/test",methods = ['POST','GET'])
 
 def test():
+    logging.info("This is the testing for my logger file")
     return "This is a testing file for ML project"
 
 if __name__=="__main__":
